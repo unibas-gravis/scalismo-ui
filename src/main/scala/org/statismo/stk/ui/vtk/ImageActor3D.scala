7d4a210e36b8653eaa68c74c1f5da2e1a889f407
Christoph Langguth <christoph.langguth@unibas.ch>
2014-09-29 14:50:56 +0200

    Image slices in 3D views are properly redrawn when slicing position was changed. Fixes #4

--- src/main/scala/org/statismo/stk/ui/vtk/ImageActor3D.scala
+++ src/main/scala/org/statismo/stk/ui/vtk/ImageActor3D.scala
@@ -1,6 +1,7 @@
 package org.statismo.stk.ui.vtk
 
 import org.statismo.stk.core.utils.{Benchmark, ImageConversion}
+import org.statismo.stk.ui.vtk.VtkContext.RenderRequest
 
 import org.statismo.stk.ui.{Axis, Image3D}
 
@@ -10,10 +11,20 @@ class ImageActor3D(source: Image3D[_])(implicit viewport: VtkViewport) extends R
   val x = ImageActor2D(source, points, Axis.X)
   val y = ImageActor2D(source, points, Axis.Y)
   val z = ImageActor2D(source, points, Axis.Z)
+
+  deafTo(this)
+  listenTo(x,y,z)
+
+  reactions += {
+    // simply forward render requests
+    case RenderRequest(ctx, imm) =>
+      publishEdt(RenderRequest(this, immediately = imm))
+  }
+
   override lazy val vtkActors: Seq[ImageActor2D] = Seq(x, y, z)
 
   override def onDestroy() = this.synchronized {
-    deafTo(viewport.interactor, source.scene)
+    deafTo(viewport.interactor, source.scene, x, y, z)
     super.onDestroy()
     vtkActors.foreach(_.onDestroy())
   }
