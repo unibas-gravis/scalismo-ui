954d11b48923fe5f42774dac93c2edf7c1f68f9f
Christoph Langguth <christoph.langguth@unibas.ch>
2015-01-30 11:42:08 +0100

    use parametricellipsoid for SphereActor

--- src/main/scala/org/statismo/stk/ui/vtk/SphereActor.scala
+++ src/main/scala/org/statismo/stk/ui/vtk/SphereActor.scala
@@ -1,16 +1,23 @@
 package org.statismo.stk.ui.vtk
 
-import vtk.vtkSphereSource
+import vtk.{vtkParametricFunctionSource, vtkParametricEllipsoid, vtkSphereSource}
 import org.statismo.stk.ui.visualization.{VisualizationProperty, SphereLike}
 
 class SphereActor(source: SphereLike) extends PolyDataActor with ColorableActor {
-  private lazy val sphere = new vtkSphereSource
+//  private lazy val sphere = new vtkSphereSource
+  private lazy val sphere = new vtkParametricEllipsoid
+  private lazy val sphereSource = new vtkParametricFunctionSource
+
+  sphereSource.SetParametricFunction(sphere)
+  sphereSource.Update()
+
   override lazy val color = source.color
   override lazy val opacity = source.opacity
   lazy val radius = source.radius
   listenTo(source, radius)
 
-  mapper.SetInputConnection(sphere.GetOutputPort())
+//  mapper.SetInputConnection(sphere.GetOutputPort())
+  mapper.SetInputConnection(sphereSource.GetOutputPort())
   this.GetProperty().SetInterpolationToGouraud()
   setGeometry()
 
@@ -20,9 +27,15 @@ class SphereActor(source: SphereLike) extends PolyDataActor with ColorableActor 
   }
 
   def setGeometry() = this.synchronized {
-    sphere.SetCenter(source.center(0), source.center(1), source.center(2))
-    sphere.SetRadius(source.radius.value)
+//    sphere.S
+//    sphere.SetCenter(source.center(0), source.center(1), source.center(2))
+
+    this.SetPosition(source.center(0), source.center(1), source.center(2))
+    sphere.SetXRadius(source.radius.value)
+    sphere.SetYRadius(source.radius.value)
+    sphere.SetZRadius(source.radius.value)
     sphere.Modified()
+    sphereSource.Modified()
     mapper.Modified()
     publishEdt(VtkContext.RenderRequest(this))
   }
@@ -31,5 +44,6 @@ class SphereActor(source: SphereLike) extends PolyDataActor with ColorableActor 
     deafTo(source, radius)
     super.onDestroy()
     sphere.Delete()
+    sphereSource.Delete()
   }
 }
