f8260c6e65d43f4bb741af7782f2adeb58baa1f6
Christoph Langguth <christoph.langguth@unibas.ch>
2015-03-30 12:20:28 +0200

    Reading images now uses new scalismo feature to determine type

--- src/main/scala/scalismo/ui/StaticImage3D.scala
+++ src/main/scala/scalismo/ui/StaticImage3D.scala
@@ -6,6 +6,7 @@ import scalismo.common.Scalar
 import scalismo.geometry._3D
 import scalismo.image.DiscreteScalarImage
 import scalismo.io.ImageIO
+import scalismo.io.ImageIO.ScalarType
 import scalismo.ui.Reloadable.{ FileReloader, ImmutableReloader, Reloader }
 import spire.math.{ ULong, UByte, UInt, UShort }
 
@@ -24,13 +25,8 @@ object StaticImage3D extends SceneTreeObjectFactory[StaticImage3D[_]] with FileI
   }
 
   def createFromFile(file: File, parent: Option[StaticThreeDObject], name: String)(implicit scene: Scene): Try[StaticImage3D[_]] = {
-    // We don't know what data type the image is, so we try them all. There should be a cleaner solution to this sooner or later
-    // (like: asking the framework what the type is). For now, we just try the most commonly used types first...
 
-    import scala.language.implicitConversions
-    implicit def functionToPartialFunction[I, O](f: I => O): PartialFunction[I, O] = { case i => f(i) }
-
-    def loadAs[T: Scalar: TypeTag: ClassTag](ignored: Throwable): Try[StaticImage3D[T]] = {
+    def loadAs[T: Scalar: TypeTag: ClassTag]: Try[StaticImage3D[T]] = {
       val reloaderTry = Try {
         new FileReloader[DiscreteScalarImage[_3D, T]](file) {
           override def doLoad() = ImageIO.read3DScalarImage[T](file)
@@ -39,18 +35,21 @@ object StaticImage3D extends SceneTreeObjectFactory[StaticImage3D[_]] with FileI
       reloaderTry.map(r => new StaticImage3D(r, parent, Some(name)))
     }
 
-    // the order is somewhat arbitrary, but expected to be roughly in
-    // descending order of "encountering such images in the wild"
-    loadAs[Short](null)
-      .recoverWith(loadAs[Float] _)
-      .recoverWith(loadAs[Double] _)
-      .recoverWith(loadAs[Int] _)
-      .recoverWith(loadAs[Byte] _)
-      .recoverWith(loadAs[Long] _)
-      .recoverWith(loadAs[UShort] _)
-      .recoverWith(loadAs[UInt] _)
-      .recoverWith(loadAs[UByte] _)
-      .recoverWith(loadAs[ULong] _)
+    ScalarType.ofFile(file) match {
+      case Success(ScalarType.Byte) => loadAs[Byte]
+      case Success(ScalarType.Short) => loadAs[Short]
+      case Success(ScalarType.Int) => loadAs[Int]
+      case Success(ScalarType.Long) => loadAs[Long]
+      case Success(ScalarType.Float) => loadAs[Float]
+      case Success(ScalarType.Double) => loadAs[Double]
+      case Success(ScalarType.UByte) => loadAs[UByte]
+      case Success(ScalarType.UShort) => loadAs[UShort]
+      case Success(ScalarType.UInt) => loadAs[UInt]
+      case Success(ScalarType.ULong) => loadAs[ULong]
+      case Failure(e) => Failure(e)
+      case _ => Failure(new IllegalStateException("won't happen")) // this is just here to please the compiler
+    }
+
   }
 
   def createFromPeer[S: Scalar: ClassTag: TypeTag](peer: DiscreteScalarImage[_3D, S], parent: Option[StaticThreeDObject] = None, name: Option[String] = None)(implicit scene: Scene): StaticImage3D[S] = {
