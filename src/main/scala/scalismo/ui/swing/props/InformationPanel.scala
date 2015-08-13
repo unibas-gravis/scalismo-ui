98993d9ffbca7b0d66e955497d196ad5247c8819
Christoph Langguth <christoph.langguth@unibas.ch>
2015-08-14 01:33:55 +0200

    WIP: Image information (incomplete work in progress)

--- src/main/scala/scalismo/ui/swing/props/InformationPanel.scala
+++ src/main/scala/scalismo/ui/swing/props/InformationPanel.scala
@@ -2,17 +2,22 @@ package scalismo.ui.swing.props
 
 import javax.swing.border.TitledBorder
 
+import scalismo.common.Scalar
+import scalismo.geometry.{ Index, Point, Vector, _3D }
+import scalismo.image.DiscreteScalarImage
 import scalismo.mesh.TriangleMesh
 import scalismo.ui.visualization.props.HasScalarRange
-import scalismo.ui.{ MeshView, ScalarMeshFieldView }
+import scalismo.ui.{ Image3DView, MeshView, ScalarMeshFieldView }
 
 import scala.reflect.ClassTag
-import scala.swing.{ Alignment, BorderPanel, GridPanel, Label }
+import scala.reflect.runtime.universe.TypeTag
+import scala.swing.GridBagPanel.{ Anchor, Fill }
+import scala.swing._
 
 class InformationPanel extends BorderPanel with PropertyPanel {
   override def description: String = "Information"
 
-  val providers: List[InformationProvider] = List(MeshInformationProvider, MeshFieldInformationProvider, ScalarRangeInformationProvider)
+  val providers: List[InformationProvider] = List(MeshInformationProvider, MeshFieldInformationProvider, ScalarRangeInformationProvider, Image3DInformationProvider)
 
   var target: Option[AnyRef] = None
 
@@ -47,12 +52,48 @@ class InformationPanel extends BorderPanel with PropertyPanel {
       horizontalAlignment = Alignment.Left
     }
 
+    def limit(s: String, maxLength: Int): String = {
+      // ellipses add 3 again, so don't uselessly trim
+      if (s.length < maxLength + 3) s
+      else {
+        s"${s.substring(0, maxLength)}..."
+      }
+    }
+
+    class InfoPanel extends GridBagPanel {
+      def add(key: String, value: String, row: Int): Unit = {
+        val kc = constraintsFor(0, row)
+        val vc = constraintsFor(1, row)
+        super.add(label(key), kc)
+        super.add(label(limit(value, 40)), vc)
+      }
+
+      def constraintsFor(x: Int, y: Int): Constraints = {
+        val c = new Constraints
+        c.gridx = x
+        c.gridy = y
+        if (x == 0) {
+          // key
+          c.anchor = Anchor.LineEnd
+          c.ipadx = 15
+          c.weightx = 0.0
+        } else {
+          //value
+          c.anchor = Anchor.LineStart
+          c.fill = Fill.Horizontal
+          c.weightx = 1.0
+        }
+        c
+      }
+
+    }
+
     val data = info.getInformation(target.get)
-    val inner = new GridPanel(data.size, 2)
+    val inner = new InfoPanel
     inner.border = new TitledBorder(info.title)
-    data.foreach {
-      case (key, value) =>
-        inner.contents ++= Seq(label(key), label(value))
+    data.zipWithIndex.foreach {
+      case ((key, value), row) =>
+        inner.add(key, value, row)
     }
     layout(inner) = BorderPanel.Position.Center
   }
@@ -63,7 +104,7 @@ trait InformationProvider {
 
   def supports(obj: AnyRef): Boolean
 
-  def getInformation(obj: AnyRef): Map[String, String]
+  def getInformation(obj: AnyRef): List[(String, String)]
 }
 
 abstract class TypedInformationProvider[T <: AnyRef: ClassTag] extends InformationProvider {
@@ -76,38 +117,81 @@ abstract class TypedInformationProvider[T <: AnyRef: ClassTag] extends Informati
     clazz.isInstance(obj)
   }
 
-  final override def getInformation(obj: AnyRef): Map[String, String] = {
+  final override def getInformation(obj: AnyRef): List[(String, String)] = {
     val t = obj.asInstanceOf[T]
     about(t)
   }
 
-  def about(t: T): Map[String, String]
+  def about(t: T): List[(String, String)]
+
+  // round with a maximum of 2 after-comma digits
+  private def r(f: Float): String = f"$f%01.02f"
 
   implicit def numberToString[V <: AnyVal](v: V): String = v.toString
+
+  implicit def vector3DToString(v: Vector[_3D]): String = s"(${r(v(0))}, ${r(v(1))}, ${r(v(2))})"
+
+  implicit def point3DToString(v: Point[_3D]): String = s"(${r(v(0))}, ${r(v(1))}, ${r(v(2))})"
+
+  implicit def index3DToString(v: Index[_3D]): String = s"(${v(0)}, ${v(1)}, ${v(2)})"
 
 }
 
 object MeshInformationProvider extends TypedInformationProvider[MeshView] {
   override val title = "Mesh"
 
-  override def about(t: MeshView): Map[String, String] = meshInfo(t.source)
+  override def about(t: MeshView): List[(String, String)] = meshInfo(t.source)
 
-  def meshInfo(t: TriangleMesh): Map[String, String] = {
-    Map("Number of points" -> t.points.toSeq.length, "Number of cells" -> t.cells.length)
+  def meshInfo(t: TriangleMesh): List[(String, String)] = {
+    List("Number of points" -> t.points.toSeq.length, "Number of cells" -> t.cells.length)
   }
 }
 
 object MeshFieldInformationProvider extends TypedInformationProvider[ScalarMeshFieldView] {
   override val title = "Mesh"
 
-  override def about(t: ScalarMeshFieldView): Map[String, String] = MeshInformationProvider.meshInfo(t.source.mesh)
+  override def about(t: ScalarMeshFieldView): List[(String, String)] = MeshInformationProvider.meshInfo(t.source.mesh)
 }
 
 object ScalarRangeInformationProvider extends TypedInformationProvider[HasScalarRange] {
 
   override def title: String = "Scalar Values"
 
-  override def about(t: HasScalarRange): Map[String, String] = {
-    Map("Minimum" -> t.scalarRange.value.absoluteMinimum, "Maximum" -> t.scalarRange.value.absoluteMaximum)
+  override def about(t: HasScalarRange): List[(String, String)] = {
+    List("Minimum" -> t.scalarRange.value.absoluteMinimum, "Maximum" -> t.scalarRange.value.absoluteMaximum)
   }
+}
+
+// FIXME: this is NOT finished yet. It shows incomplete/wrong information.
+object Image3DInformationProvider extends TypedInformationProvider[Image3DView[_]] {
+  override def about(view: Image3DView[_]): List[(String, String)] = {
+    val pt = view.pixelType
+
+    def treatAs[T: Scalar: ClassTag: TypeTag]: List[(String, String)] = {
+      val img = view.source.asInstanceOf[DiscreteScalarImage[_3D, T]]
+
+      //      def minMax(): (T,T) = {
+      def minMax(): (String, String) = {
+        ("???", "???")
+      }
+
+      val (min, max) = minMax()
+
+      val domain = img.domain
+      val bb = domain.boundingBox
+      List(
+        "Pixel Type" -> pt.toString,
+        "Origin" -> bb.origin,
+        "Limit" -> bb.oppositeCorner,
+        "Extent" -> bb.extent,
+        "Spacing" -> domain.spacing,
+        "Discrete size" -> domain.size,
+        "Scalar range" -> s"($min, $max)"
+      )
+    }
+    // FIXME: this is (knowingly) wrong.
+    treatAs[Float]
+  }
+
+  override def title: String = "Image"
 }
