a6c4f0d675bf8342aa9f70795f22790912ccadb5
Christoph Langguth <christoph.langguth@unibas.ch>
2016-03-08 04:31:42 +0100

    Fix: Apache Software Foundation

--- main/scala/scalismo/ui/resources/thirdparty/batik/Batik.scala
+++ main/scala/scalismo/ui/resources/thirdparty/batik/Batik.scala
@@ -3,7 +3,7 @@ package scalismo.ui.resources.thirdparty.batik
 import scalismo.ui.resources.thirdparty.ThirdPartyResource
 
 object Batik extends ThirdPartyResource {
-  override def authors: String = "The Apache Software Foundation"
+  override def authors: String = "Apache Software Foundation"
 
   override def licenseName: String = "Apache 2.0"
 
