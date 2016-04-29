c1e6bad7d8db298ef44c49c1856b26153d082de9
Marcel Luethi <marcel.luethi@unibas.ch>
2016-04-29 13:41:02 +0200

    optional window title in ScalismoUI

--- src/main/scala/scalismo/ui/api/ScalismoUI.scala
+++ src/main/scala/scalismo/ui/api/ScalismoUI.scala
@@ -4,13 +4,16 @@ import scalismo.ui.control.interactor.{ DefaultInteractor, Interactor }
 import scalismo.ui.util.EdtUtil
 import scalismo.ui.view.ScalismoFrame
 
-class ScalismoUI() extends SimpleAPI {
+class ScalismoUI(title : String) extends SimpleAPI {
 
   private[ui] val frame = EdtUtil.onEdtWait {
     val frame = new ScalismoFrame()
+
     frame.setup(Array[String]())
     frame.pack()
     frame.visible = true
+
+    frame.title = title;
     frame
   }
 
@@ -20,9 +23,9 @@ class ScalismoUI() extends SimpleAPI {
 
 object ScalismoUI {
 
-  def apply(interactor: Interactor = new DefaultInteractor {}): ScalismoUI = {
+  def apply(title : String = ""): ScalismoUI = {
     scalismo.initialize()
-    new ScalismoUI()
+    new ScalismoUI(title)
   }
 }
 
