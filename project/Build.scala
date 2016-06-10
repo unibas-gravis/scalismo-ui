ed3958fb92557488caec640333acc3a7244619f2
Marcel Luethi <marcel.luethi@unibas.ch>
2016-06-10 08:38:07 +0200

    reset version to develop-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import sbtbuildinfo.Plugin._
 
 object BuildSettings {
   val buildOrganization = "ch.unibas.cs.gravis"
-  val buildVersion = "0.8.0-RC2"
+  val buildVersion = "develop-SNAPSHOT"
   val buildScalaVersion = "2.11.8"
   val publishURL = Resolver.file("file", new File("/export/contrib/statismo/repo/private"))
 
