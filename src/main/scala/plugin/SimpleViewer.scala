05a16a7869b901a98723de0d71d438d03167df44
Ghazi Bouabene <ghazi.bouabene@unibas.ch>
2016-07-27 15:57:09 +0200

    further fixes to 2D camera rotation

--- src/main/scala/plugin/SimpleViewer.scala
+++ src/main/scala/plugin/SimpleViewer.scala
@@ -15,9 +15,6 @@ class SimpleViewer extends ScalismoFrame {
   override def setup(args: Array[String]): Unit = {
     super.setup(args)
 
-    val group = scene.groups.add("a group")
-    val model = StatismoIO.readStatismoMeshModel(new File("/home/bouabene/000/femurMooc.h5")).get
-
     //    group.addStatisticalMeshModel(model, "face")
 
     //    val meshField = ScalarMeshField(model.referenceMesh, ScalarArray(model.referenceMesh.pointIds.map(_.id.toFloat).toArray))
@@ -25,22 +22,7 @@ class SimpleViewer extends ScalismoFrame {
 
     perspective.resetAllCameras()
 
-    // val smt = ShapeModelTransformation(RigidTransformationSpace[_3D].transformForParameters(DenseVector(1,1,1,1,1,1)), DiscreteLowRankGpPointTransformation(model.gp))
-
-    group.shapeModelTransformations.addPoseTransformation(RigidTransformationSpace[_3D].transformForParameters(DenseVector(1, 1, 1, 1, 1, 1)), "shapemodel rigid")
-    group.shapeModelTransformations.addGaussianProcessTransformation(DiscreteLowRankGpPointTransformation(model.gp), "shapemmodel GP")
-
-    // try to add the transform again
-    group.shapeModelTransformations.addGaussianProcessTransformation(DiscreteLowRankGpPointTransformation(model.gp), "shapemmodel GP")
-
-    group.triangleMeshes.add(model.referenceMesh, "ref")
-
-    group.genericTransformations.add(RigidTransformationSpace[_3D].transformForParameters(DenseVector(1, 1, 1, 1, 1, 1)), "more rigid")
-
-    group.genericTransformations.add((pt: Point[_3D]) => model.referenceMesh.pointSet.findClosestPoint(pt).point, "closest Point proj")
-
     val staticGroup = scene.groups.add("static")
-    staticGroup.triangleMeshes.add(model.referenceMesh, "a")
 
   }
 }
