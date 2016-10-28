7a2fcbdd070a2da6894343e425178254b7aa782c
Ghazi Bouabene <ghazi.bouabene@unibas.ch>
2016-10-28 19:07:03 +0200

    added visibility config

--- src/main/scala/scalismo/ui/api/SimpleVisibility.scala
+++ src/main/scala/scalismo/ui/api/SimpleVisibility.scala
@@ -3,18 +3,55 @@ package scalismo.ui.api
 import scalismo.ui.model.capabilities.RenderableSceneNode
 
 /**
- * Trait to be mixed with ObjectView instances in order to provide visibility changing functionality
- *
- */
+  * Trait to be mixed with ObjectView instances in order to provide visibility changing functionality
+  *
+  */
 
-sealed trait SimpleViewport
+sealed trait Viewport {
+  val name: String
+}
 
-case object xView extends SimpleViewport
-case object yView extends SimpleViewport
-case object zView extends SimpleViewport
-case object _3DLeft extends SimpleViewport
-case object _3DRight extends SimpleViewport
-case object _3D extends SimpleViewport
+case object xView extends Viewport {
+  override val name = "X"
+}
+
+case object yView extends Viewport {
+  override val name = "Y"
+}
+
+case object zView extends Viewport {
+  override val name = "Z"
+}
+
+case object _3DLeft extends Viewport {
+  override val name = "Left"
+}
+
+case object _3DRight extends Viewport {
+  override val name = "Right"
+}
+
+case object _3D extends Viewport {
+  override val name = "3D"
+}
+
+object Viewport {
+
+  val all = Seq(xView, yView, zView, _3D, _3DLeft, _3DRight)
+  val none = Seq()
+  val xOnly: Seq[Viewport] = Seq(xView)
+  val yOnly: Seq[Viewport] = Seq(yView)
+  val zOnly: Seq[Viewport] = Seq(zView)
+  val _2dOnly: Seq[Viewport] = Seq(xView, yView, zView)
+  val _3dOnly: Seq[Viewport] = Seq(_3D, _3DRight, _3DLeft)
+  val _3dLeft: Seq[Viewport] = Seq(_3DLeft)
+  val _3dRight: Seq[Viewport] = Seq(_3DRight)
+}
+
+sealed trait VisibilityConfig {
+  val views: Seq[Viewport]
+}
+
 
 trait SimpleVisibility {
   self: ObjectView =>
@@ -25,11 +62,15 @@ trait SimpleVisibility {
     visib.setVisibility(peer.asInstanceOf[RenderableSceneNode], viewPort, isVisible)
   }
 
-  //def visible()
+  /**
+    * Sets the node visible to all views in the config, and invisible in others
+    **/
+  def visible_=(config: Seq[Viewport]) = {
+    config.foreach(v => setVisible(true, v.name))
 
-  def visible3D(isVisible: Boolean) = setVisible(isVisible, "3D")
-  def visibleX(isVisible: Boolean) = setVisible(isVisible, "X")
-  def visibleY(isVisible: Boolean) = setVisible(isVisible, "Y")
-  def visibleZ(isVisible: Boolean) = setVisible(isVisible, "Z")
+    //set invisible in other viewports
+    val viewsNotInConfig = self.frame.perspective.viewports.filterNot(v => config.exists(_.name == v.name))
+    viewsNotInConfig.foreach(v => setVisible(false, v.name))
+  }
 
 }
