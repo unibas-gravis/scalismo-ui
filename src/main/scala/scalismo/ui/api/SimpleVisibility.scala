9b953a2cf08217a2b7217d5b78f23e3f5620a916
Ghazi Bouabene <ghazi.bouabene@unibas.ch>
2016-10-31 17:33:22 +0100

    added visibility assignment operator

--- src/main/scala/scalismo/ui/api/SimpleVisibility.scala
+++ src/main/scala/scalismo/ui/api/SimpleVisibility.scala
@@ -1,11 +1,10 @@
 package scalismo.ui.api
-
 import scalismo.ui.model.capabilities.RenderableSceneNode
 
 /**
-  * Trait to be mixed with ObjectView instances in order to provide visibility changing functionality
-  *
-  */
+ * Trait to be mixed with ObjectView instances in order to provide visibility changing functionality
+ *
+ */
 
 sealed trait Viewport {
   val name: String
@@ -31,19 +30,19 @@ case object _3DRight extends Viewport {
   override val name = "Right"
 }
 
-case object _3D extends Viewport {
+case object _3DMain extends Viewport {
   override val name = "3D"
 }
 
 object Viewport {
 
-  val all = Seq(xView, yView, zView, _3D, _3DLeft, _3DRight)
+  val all = Seq(xView, yView, zView, _3DMain, _3DLeft, _3DRight)
   val none = Seq()
   val xOnly: Seq[Viewport] = Seq(xView)
   val yOnly: Seq[Viewport] = Seq(yView)
   val zOnly: Seq[Viewport] = Seq(zView)
   val _2dOnly: Seq[Viewport] = Seq(xView, yView, zView)
-  val _3dOnly: Seq[Viewport] = Seq(_3D, _3DRight, _3DLeft)
+  val _3dOnly: Seq[Viewport] = Seq(_3DMain, _3DRight, _3DLeft)
   val _3dLeft: Seq[Viewport] = Seq(_3DLeft)
   val _3dRight: Seq[Viewport] = Seq(_3DRight)
 }
@@ -52,19 +51,26 @@ sealed trait VisibilityConfig {
   val views: Seq[Viewport]
 }
 
-
 trait SimpleVisibility {
   self: ObjectView =>
 
   private def setVisible(isVisible: Boolean, viewportName: String): Unit = {
-    val viewPort = self.frame.perspective.viewports.filter(_.name == viewportName).head
+    self.frame.perspective.viewports.filter(_.name == viewportName).headOption.foreach { viewPort =>
+      val visib = self.frame.sceneControl.nodeVisibility
+      visib.setVisibility(peer.asInstanceOf[RenderableSceneNode], viewPort, isVisible)
+    }
+  }
+
+  // mainly needed to be able to have the assignment operator
+  def visible: Seq[Viewport] = {
     val visib = self.frame.sceneControl.nodeVisibility
-    visib.setVisibility(peer.asInstanceOf[RenderableSceneNode], viewPort, isVisible)
+    val visibleViewPortNames = self.frame.perspective.viewports.filter(vp => visib.isVisible(peer.asInstanceOf[RenderableSceneNode], vp)).map(_.name)
+    Viewport.all.filter(p => visibleViewPortNames.contains(p.name))
   }
 
   /**
-    * Sets the node visible to all views in the config, and invisible in others
-    **/
+   * Sets the node visible to all views in the config, and invisible in others
+   */
   def visible_=(config: Seq[Viewport]) = {
     config.foreach(v => setVisible(true, v.name))
 
