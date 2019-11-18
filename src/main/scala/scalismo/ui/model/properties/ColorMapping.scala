7d1931cfdf3d63b8c4352d31cf8a6e201bf204b7
Dennis Madsen <dennis.madsen@unibas.ch>
2019-11-18 08:39:40 +0100

    Color range picker for scalar meshes
    Co-authored-by: DanaRahbani <dana.rahbani@unibas.ch>

---
+++ src/main/scala/scalismo/ui/model/properties/ColorMapping.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2016  University of Basel, Graphics and Vision Research Group 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package scalismo.ui.model.properties
+
+import java.awt.Color
+
+import scalismo.color.RGB
+
+/** Maps a range of values, determined by a lower and upper value to a color */
+trait ColorMapping {
+
+  var lowerColor: Color
+
+  var upperColor: Color
+
+  def mappingFunction(scalarRange: ScalarRange): (Double => Color)
+
+  def suggestedNumberOfColors: Int
+}
+
+private[properties] case class LinearColorMapping(lColor: Color, uColor: Color) extends ColorMapping {
+
+  override var lowerColor: Color = lColor
+  override var upperColor: Color = uColor
+
+  override def mappingFunction(scalarRange: ScalarRange): (Double => Color) = {
+    value =>
+      {
+
+        val lowerValue = scalarRange.cappedMinimum
+        val upperValue = scalarRange.cappedMaximum
+        if (value < lowerValue) lColor
+        else if (value > upperValue) uColor
+        else {
+          val s = (value - lowerValue) / (upperValue - lowerValue)
+          val newColor = (RGB(uColor) - RGB(lColor)) * s + RGB(lColor)
+          newColor.toAWTColor
+        }
+      }
+  }
+
+  override val suggestedNumberOfColors = 100
+
+}
+
+object BlueToRedColorMapping extends LinearColorMapping(Color.BLUE, Color.RED)
+
+object BlackToWhiteMapping extends LinearColorMapping(Color.BLACK, Color.WHITE)
+
+object WhiteToBlackMapping extends LinearColorMapping(Color.WHITE, Color.BLACK)
+
---
+++ src/main/scala/scalismo/ui/view/util/FancyDropDown.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2016  University of Basel, Graphics and Vision Research Group 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package scalismo.ui.view.util
+
+import java.awt.event.ActionEvent
+import javax.swing.JComboBox
+
+class FancyDropDown(items: Array[String])
+    extends scala.swing.Component with java.awt.event.ActionListener {
+
+  def formattedValue(dropDownValue: Int): String = dropDownValue.toString
+
+  override lazy val peer = new JComboBox(items)
+
+  def +=(item: String): Unit = peer.addItem(item)
+  def -=(item: String): Unit = peer.removeItem(item)
+  def item: String = peer.getSelectedItem.asInstanceOf[String]
+  def reset() {
+    peer.setSelectedIndex(0)
+  }
+
+  peer.addActionListener(this)
+  def actionPerformed(e: ActionEvent): Unit = {
+
+  }
+
+}
